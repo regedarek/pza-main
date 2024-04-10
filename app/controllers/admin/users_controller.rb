0fee8790d48e534aab47b51ed44a34ffcf2a3460
Darek Finster <dariusz.finster@gmail.com>
2024-04-10 22:20:07 +0200

    cleanup

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -61,12 +61,12 @@ module Admin
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_user
-        @user = User.find(params[:id])
+        @user = User.friendly.find(params[:id])
       end
 
       # Only allow a list of trusted parameters through.
       def user_params
-        params.require(:user).permit(:first_name, :last_name_string, :email)
+        params.require(:user).permit(:first_name, :last_name, :email)
       end
   end
 end
