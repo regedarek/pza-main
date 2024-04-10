1efa8ca65f220bb29df02744902b66ecebdfe98e
Darek Finster <dariusz.finster@gmail.com>
2024-04-10 10:42:56 +0200

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :uuid             not null, primary key
+#  email            :string
+#  first_name       :string
+#  last_name_string :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class User < ApplicationRecord
 end
