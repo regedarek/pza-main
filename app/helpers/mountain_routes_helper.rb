6dc8feb40bbdebbf2461c09b4f415babe362ffad
Darek Finster <dariusz.finster@gmail.com>
2024-04-11 12:01:58 +0200

    team link

--- app/helpers/mountain_routes_helper.rb
+++ app/helpers/mountain_routes_helper.rb
@@ -1,5 +1,12 @@
 module MountainRoutesHelper
   def team_link(route)
-    [link_to(route.user.name, user_path(route.user)), route.partner].reject(&:empty?).compact.join(' + ').strip
+    [
+      link_to(
+        route.user.name,
+        user_path(route.user),
+        class: "rounded-lg text-blue-500 inline-block font-medium"
+      ),
+      route.partner
+    ].reject(&:empty?).compact.join(' + ').strip
   end
 end
